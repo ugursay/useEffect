e836c1225f8a5f107d14d89d1ff48e4b834b5c0f
Uğur <ugursay3211@gmail.com>
2024-06-19 13:26:03 +0300

    useEffect
    This app is about useEffect

---
+++ use-effect/src/App.js
@@ -0,0 +1,46 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  useEffect(() => {
+    console.log("Her zaman çalışır.");
+  });
+  useEffect(() => {
+    console.log("İlk Render edildiğinde çalışır.");
+  }, []);
+  useEffect(() => {
+    console.log(
+      "İlk Render edildiğinde ve Firstname ya da Lastname değiştiğinde çalışır."
+    );
+  }, [firstName, lastName]);
+
+  const isimDegistir = () => {
+    setFirstName("uğur");
+  };
+
+  return (
+    <div>
+      <div>
+        {firstName} <br></br> {lastName}
+      </div>
+      <div>
+        <button onClick={isimDegistir}>Ad Değiştir</button>
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            setLastName("Sayan");
+          }}
+        >
+          Soyad Değiştir
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
